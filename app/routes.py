d047ec176aefd46fb5360ba701c9fb4de1fe0256
rodriguesflavio1 <rodriguesflavio1@gmail.com>
2024-10-22 18:19:48 -0300

    criando um usuario default caso o usuario não passe o parametro

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 
 @app.route('/') # definindo uma rota para url do portal
-@app.route('/index/<nome>') # criando uma rota alternativa index para url do portal
+@app.route('/index/<nome>',defaults="nome":"usuario") # criando uma rota alternativa index para url do portal
 def index(nome):
     dados = {"profissao": "Analista Desenvolvedor","Nivel": "Senior"} 
     return render_template('index.html',nome= nome, dados= dados) # usando a função render_template para abrir arquivo html externo
