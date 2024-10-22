62a07a94a1d8f6474432880e8a3e008e5dc291b0
rodriguesflavio1 <rodriguesflavio1@gmail.com>
2024-10-22 18:50:14 -0300

    .

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 
 
-@app.route('/') # definindo uma rota para url do portal
+@app.route('/index') # definindo uma rota para url do portal
 #@app.route('/index', defaults={"nome":"usuario"})
 #@app.route('/index/<nome>') # criando uma rota alternativa index para url do portal
 def index():
@@ -14,6 +14,6 @@ def index():
 def contato():
     return render_template('contato.html') # usando a função render_template para abrir arquivo html externo
 
-@app.route('/login')
+@app.route('/login') # definindo uma rota para url do portal
 def login():
     return render_template('login.html')
